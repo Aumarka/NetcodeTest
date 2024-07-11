30a4359e13759fc9f8c58fd6e7fdb7511cc6306d
Mark Auman <markryanauman@gmail.com>
2024-07-11 18:00:38 +1000

    Added IP Address Display Script

---
+++ Assets/DisplayIPAddress.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+public class DisplayIPAddress : MonoBehaviour
+{
+    void Start()
+    {
+        string localIP = GetLocalIPAddress();
+        if (localIP != null)
+        {
+            Debug.Log("Local IP Address: " + localIP);
+        }
+        else
+        {
+            Debug.Log("Unable to retrieve the local IP address.");
+        }
+    }
+
+    string GetLocalIPAddress()
+    {
+        try
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Exception caught while retrieving IP address: " + ex.Message);
+        }
+
+        return null;
+    }
+}
